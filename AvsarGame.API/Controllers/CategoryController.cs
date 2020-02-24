094206d1faaff0f58170d8c88f2c2f0490556b07
esats <esat.avsr@gmail.com>
2020-02-24 19:08:13 +0300

    modal yapıldı

---
+++ AvsarGame.API/Controllers/CategoryController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using AvsarGame.API.Base;
+using AvsarGame.API.Models;
+using AvsarGame.Dal.Abstract;
+using AvsarGame.Entities.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AvsarGame.API.Controllers {
+    [Route("api/Category")]
+    public class CategoryController : APIControllerBase {
+        private readonly ICategory _category;
+
+        public CategoryController(ICategory category) {
+            _category = category;
+        }
+
+        public ActionResult Save([FromBody] CategoryModel model) {
+            try {
+                Category entity = new Category() {
+                        //ImageUrl = model.ImageUrl,
+                        //CreatedBy = base yapısından giriş yapan kullanıcıyı al ve ona göre createdbyı getir. mastan base sistemini alabiliriz.
+                };
+            } catch (Exception e) {
+                return StatusCode(404);
+            }
+
+            return StatusCode(200);
+        }
+    }
+}
