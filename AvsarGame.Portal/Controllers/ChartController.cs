96dd679786f10e2d724b3e381b49aa74c7fdb0e6
esats <esat.avsr@gmail.com>
2020-03-20 02:10:12 +0300

    CHART SUMMARY CREATE

--- AvsarGame.Portal/Controllers/ChartController.cs
+++ AvsarGame.Portal/Controllers/ChartController.cs
@@ -5,51 +5,54 @@ using System.Threading.Tasks;
 using AvsarGame.API.Models;
 using AvsarGame.Portal.Core;
 using AvsarGame.Portal.Helpers;
+using HtmlAgilityPack;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 
 namespace AvsarGame.Portal.Controllers {
     [Route("Sepetim")]
     public class ChartController : Controller {
-
         public IActionResult Index() {
             var chart = SessionManager.Instance.GetObject<List<Guid>>("chart");
             if (chart == null) {
                 return null;
             }
+
             var ids = ConvertToString(chart);
-            List<GameModel> games  = JsonConvert.DeserializeObject<List<GameModel>>(UiRequestManager.Instance.Get(string.Format("Game/GetGames?gamesId={0}",ids)));
+            List<GameModel> games = JsonConvert.DeserializeObject<List<GameModel>>(UiRequestManager.Instance.Get(string.Format("Game/GetGames?gamesId={0}", ids)));
 
             return View(games);
         }
 
         [HttpPost]
         [Route("AddChart")]
-        public JsonResult AddChart(Guid id) {
-            List<Guid> products = new List<Guid>();
+        public JsonResult AddChart(GameModel model) {
+            List<GameModel> products = new List<GameModel>();
             try {
-                var chart = SessionManager.Instance.GetObject<List<Guid>>("chart");
-                if (chart?.Count(x => x == id) > 0) {
-                    return Json(new { Success = true, chart.Count});
+                var chart = SessionManager.Instance.GetObject<List<GameModel>>("chart");
+                if (chart?.Count(x => x.Id == model.Id) > 0) {
+                    return Json(new { Success = true, chart.Count });
                 } else {
                     products = chart ?? products;
-                    products.Add(id);
+                    products.Add(model);
                     SessionManager.Instance.SetObject("chart", products);
                     return Json(new { Success = true, Count = products.Count });
-
                 }
             } catch (Exception e) {
                 return Json(new { Success = false });
             }
-
         }
 
-        public string ConvertToString<T>(List<T> Items)
-        {
+        [HttpGet]
+        [Route("GetChartDetail")]
+        public JsonResult GetChartDetail() {
+            List<GameModel> list = SessionManager.Instance.GetObject<List<GameModel>>("chart");
+            return Json(new { Success = true, data = list });
+        }
+        public string ConvertToString<T>(List<T> Items) {
             string result = string.Empty;
 
-            foreach (T item in Items)
-            {
+            foreach (T item in Items) {
                 result += item.ToString() + ",";
             }
 
